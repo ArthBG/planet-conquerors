462cbfade8d47a85c76b1c29f6a0f53a8465e66a
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-03-19 20:02:30 -0300

    Add PlanetList class to manage planets

---
+++ src/models/ListPlanets.js
@@ -0,0 +1,34 @@
+export class PlanetList {
+  constructor() {
+    this.planets = [];
+  }
+
+  addPlanet(planet) {
+    this.planets.push(planet);
+  }
+
+  removePlanet(id) {
+    this.planets = this.planets.filter((planet) => planet.id !== id);
+  }
+
+  getPlanets() {
+    return this.planets;
+  }
+
+  getPlanet(name) {
+    return this.planets.find((planet) => planet.name === name);
+  }
+
+    getPlanetById(id) {
+        return this.planets.find((planet) => planet.id === id);
+    }
+
+    updatePlanet(planet) {
+        this.planets = this.planets.map((planets) => {
+            if (planets.id === planet.id) {
+                return planet;
+            }
+            return planets;
+        });
+    }
+}
