02a491a44a8c4198aa62e630148eb83227a1c139
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-03-19 19:37:30 -0300

    Remove StackRoutes and uncomment TabRoutes

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,10 +1,20 @@
 import { View, Text } from 'react-native'
 import Title from '../../components/Title'
-import styles from './styles'
+// import styles from './styles'
 export default function Home() {
   return (
     <View style={styles.container}>
         <Title title="Home" />
+        <Text>Home Screen</Text>
     </View>
   )
+
+}
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f23f',
+  },
 }
