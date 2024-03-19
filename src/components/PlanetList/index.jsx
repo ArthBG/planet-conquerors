95d4caf2e1ff8f017ec7a765df49cd2638bdf36b
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-03-19 20:05:55 -0300

    Add PlanetList component

---
+++ src/components/PlanetList/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import PlanetItem from './PlanetItem';
+
+const PlanetList = ({ planets }) => {
+  return (
+    <View>
+      {/* Planet.map */}
+    </View>
+  );
+};
+
+export default PlanetList;
