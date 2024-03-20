9f96e21e907dc3484671a6c49dc6bc823a17ce76
Arthur Borges <arthurborges.senai564@gmail.com>
2024-03-20 08:10:25 -0300

    Delete PlanetDetails component and add PlanetsC component

---
+++ src/screens/PlanetsC/index.jsx
@@ -0,0 +1,8 @@
+//Cadastro dos planetas
+import React from 'react'
+import { View, Text } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
+import Title from '../../components/Title'
+import Inputtext from '../../components/Inputtext';
+import PlanetForm from '../../components/PlanetForm';
+import PlanetItem from '../../components/PlanetItem';
