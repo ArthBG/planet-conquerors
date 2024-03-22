157adc33f930faf6e0552318f18c293068bb8a4b
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-03-22 19:58:47 -0300

    Update dependencies in package.json and package-lock.json

--- App.jsx
+++ App.jsx
@@ -1,6 +1,12 @@
 import "react-native-gesture-handler";
-
+import { AppRegistry } from "react-native";
 import Routes from "./src/routes";
+
+AppRegistry.registerComponent("App", () => Routes);
+AppRegistry.runApplication("App", {
+  initialProps: {},
+  rootTag: document.getElementById("root"),
+});
 
 export default function App() {
   return <Routes />;
