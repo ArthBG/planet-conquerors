e2b13a9a3c21aff521acf31711eb8eb58252ef0c
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-03-22 19:17:01 -0300

    Add planet list and navigation functionality to Home screen

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,10 +1,23 @@
 import { Text, TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
 import { useNavigation, useIsFocused } from "@react-navigation/native";
 import { LinearGradient } from 'expo-linear-gradient';
 import Title from '../../components/Title'
 import PlanetList from '../../models/ListPlanets';
 // import styles from './styles'
 export default function Home() {
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+  const [allPlanets, setAllPlanets] = useState([]);
+
+  useEffect(() => {
+    if (isFocused){
+      const planetList = new PlanetList();
+      setAllPlanets(planetList.getPlanets());
+    }
+  }
+  , [isFocused]);
+
   return (
     <LinearGradient
     style={styles.container}
@@ -16,7 +29,24 @@ export default function Home() {
       <View style={styles.containerText}>
         <Title title="PlanetC" color={"#fff"} size={25} />
         <Text style={styles.subtitle}>Aqui estão suas conquistas e descobertas de planetas de todo o universo</Text>
+        <View style={styles.containerPlanets}>
+          {allPlanets.length > 0 ? (
+          allPlanets.map((planet) => (
+            <TouchableOpacity
+              key={planet.id}
+              onPress={() => navigation.navigate('Planet', { id: planet.id })}
+            >
+              <View style={styles.planet}>
+                <Text style={styles.planetName}>{planet.name}</Text>
+              </View>
+            </TouchableOpacity>
+          )) 
+          ) : (
+            <Text style={styles.subtitle}>Você ainda não descobriu nenhum planeta</Text>
+          )}
+          </View>
       </View>
+
         <LinearGradient
     style={styles.containerPlanets}
     colors={['#00065F', '#1C005B']}
