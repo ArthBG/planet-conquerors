325cfc6428a6d150e02064d79059575cd1dbc32c
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-03-24 21:40:49 -0300

    Update Profile screen styles

--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -3,8 +3,9 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        margin: 10
+        textAlign: 'center',
     },
+   
     image: {
         width: 200,
         height: 200,
